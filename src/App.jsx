17f94c893e99c46d07591f23743d4951a2a56fa3
calebosam <caleb.osam@amalitech.com>
2024-10-20 00:06:24 +0000

    feat: refactor home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
-import { useState } from 'react'
-import logo from './assets/image.png'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from './pages/Home';
 
-const posts = [{id: 1, data: "happy"}]
-const books = [{id: 1, data: "joy"}]
-const users = [{id: 1, data: "sing"}]
 function App() {
-  const [items, setItems] = useState([])
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={logo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Scaling Engine</h1>
-      <div className="card">
-        <button style={{marginRight: "20px"}} onClick={() => setItems(users)}>
-          User Service
-        </button>
-        <button style={{marginRight: "20px"}}onClick={() => setItems(posts)}>
-          Posts Service
-        </button>
-        <button onClick={() => setItems(books)}>
-          Books Service
-        </button>
-        <p>
-          {items?.map((el) => <ul key={el.id}> {el.data} </ul>)}
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      
+      </Routes>
+       
+    </BrowserRouter>
+      
+    
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import logo from '../assets/image.png'
+
+const Home = () => {
+
+    const posts = [{id: 1, data: "happy"}]
+    const books = [{id: 1, data: "joy"}]
+    const users = [{id: 1, data: "sing"}]
+    const [items, setItems] = useState([])
+  return (
+    <>
+      <div>
+        <a href="https://www.linkedin.com/in/caleb-osam/" target="_blank">
+          <img src={logo} className="logo react" alt="Scaling logo" />
+        </a>
+      </div>
+      <h1>Scaling Engine</h1>
+      <div className="card">
+        <button style={{marginRight: "20px"}} onClick={() => setItems(users)}>
+          User Service
+        </button>
+        <button style={{marginRight: "20px"}}onClick={() => setItems(posts)}>
+          Posts Service
+        </button>
+        <button onClick={() => setItems(books)}>
+          Books Service
+        </button>
+        <p>
+          {items?.map((el) => <ul key={el.id}> {el.data} </ul>)}
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default Home
